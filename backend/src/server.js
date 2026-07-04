1a7b555e44f246fff634143dc5c464e5fa7c7bb4
Praveen S <praveenselvaraj2004@gmail.com>
2026-07-04 12:39:07 +0530

    Production commented

--- backend/src/server.js
+++ backend/src/server.js
@@ -28,13 +28,13 @@ app.use(rateLimit)
 
 app.use("/api/notes", noteRouters)
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-    })
-}
+//     app.get("*", (req, res) => {
+//         res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+//     })
+// }
 
 connectDB().then(() => {
     app.listen(PORT, () => {
