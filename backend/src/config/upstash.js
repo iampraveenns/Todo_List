5560cbbf71bd0f9ffd38e06dcf17ce38c3d1c272
Praveen S <praveenselvaraj2004@gmail.com>
2026-07-04 12:28:21 +0530

    modifiy

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -2,8 +2,6 @@ import { Ratelimit } from "@upstash/ratelimit"
 import { Redis } from "@upstash/redis"
 
 // import dotenv from "dotenv"
-console.log("URL:", process.env.UPSTASH_REDIS_REST_URL);
-console.log("TOKEN:", process.env.UPSTASH_REDIS_REST_TOKEN);
 // dotenv.config();
 
 const ratelimit = new Ratelimit ({
